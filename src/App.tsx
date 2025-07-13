a956620a061572f23d2b784419c0c2808458bbfc
Judy <judy@macbookpro.home>
2025-07-13 12:45:07 +0100

    Set landing page font to Georgia

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
+    <div style={{ fontFamily: 'Georgia, serif' }}>
       <p>No man is better than a machine. No machine is better than a man with a machine.</p>
       <p>Judy is a finance professional, technology enthusiast and above all curious human being</p>
     </div>
